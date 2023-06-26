704d351445a868cdb924fe32919caf7cc66086ab
imchanu <cksdn9979@gmail.com>
2023-06-26 17:09:10 +0900

    Chapter 1 수정및 재업로드

--- Ch1VariableBasic/src/AnotherEx1.java
+++ Ch1VariableBasic/src/AnotherEx1.java
@@ -4,8 +4,8 @@ public class AnotherEx1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*
-		문자열 + any type => 문자열 + 문자열 => 문자열
-		any type + 문자열 => 문자열 + 문자열 => 문자열
+		*문자열 + any type => 문자열 + 문자열 => 문자열
+		*any type + 문자열 => 문자열 + 문자열 => 문자열
 		*/
 		
 		String name = "임찬우";
